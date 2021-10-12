af7b920f3ec5dc9b7c4efefef563c043ef483663
unknown <s.alireza.m8888@gmail.com>
2021-10-12 16:07:39 +0330

    add header and footer and sidebar to layout folder

---
+++ src/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex justify-content-between align-items-center">
+        <b-nav small>
+            <b-nav-item active>Active</b-nav-item>
+            <b-nav-item>Link</b-nav-item>
+            <b-nav-item>Another Link</b-nav-item>
+            <b-nav-item disabled>Disabled</b-nav-item>
+        </b-nav>
+        <img src="@/assets/logo.png" width="50px" alt="" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
+
+<style></style>
---
+++ src/layout/SideMenu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sidebar">
+        <ul>
+            <li>Home</li>
+            <li>Wallet</li>
+            <li>Lending</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SideMenu",
+};
+</script>
+
+<style scoped>
+.sidebar ul {
+    list-style: none;
+    text-align: left;
+}
+</style>
