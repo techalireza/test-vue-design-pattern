bf070788d29b10a11ac25834a477908d2318f08e
unknown <s.alireza.m8888@gmail.com>
2021-10-12 16:10:01 +0330

    modify App and use page and layou into

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header></Header>
+        <b-row>
+            <b-col lg="4">
+                <SideMenu />
+            </b-col>
+            <b-col lg="8">
+                <Home />
+            </b-col>
+        </b-row>
+        <Footer>test</Footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./layout/Header.vue";
+import Footer from "./layout/Footer.vue";
+import SideMenu from "./layout/SideMenu.vue";
+import Home from "./views/Home.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    el: "root",
+    components: {
+        Header,
+        Footer,
+        SideMenu,
+        Home,
+    },
+    methods: {
+        debug() {
+            console.log(this.$root);
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
