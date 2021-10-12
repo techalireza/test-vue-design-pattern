35ec546ebed7dcc73fe8c0706d6649854c24c8aa
unknown <s.alireza.m8888@gmail.com>
2021-10-12 16:08:43 +0330

    add home page

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-card>
+        <b-card-text>
+            {{ user.name }}
+        </b-card-text>
+
+        <b-card-text>{{ user.username }}</b-card-text>
+
+        <a href="#" class="card-link">{{ user.email }}</a>
+    </b-card>
+</template>
+
+<script>
+import myApi from "@/api/api.js";
+export default {
+    name: "Home",
+    data() {
+        return {
+            user: {},
+        };
+    },
+    mounted() {
+        myApi.get("/users/1").then((response) => (this.user = response.data));
+    },
+};
+</script>
+
+<style></style>
